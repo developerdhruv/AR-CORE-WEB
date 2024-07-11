88bfe50b6873606281e4e9160234b77db988923d
developerdhruv <dhruvkumar9115@gmail.com>
2024-07-11 23:00:57 +0530

    index left

--- chitradrish/main.js
+++ chitradrish/main.js
@@ -1,24 +1,27 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+async function uploadImage() {
+  const input = document.getElementById('imageUpload');
+  const file = input.files[0];
+  if (!file) {
+      alert("Please select a file");
+      return;
+  }
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+  const formData = new FormData();
+  formData.append('file', file);
 
-setupCounter(document.querySelector('#counter'))
+  try {
+      const response = await axios.post('http://localhost:8000/predict', formData, {
+          responseType: 'blob'
+      });
+
+      // Create a URL for the blob
+      const blob = response.data;
+      const url = URL.createObjectURL(blob);
+
+      // Set the URL to the model entity
+      const entity = document.getElementById('ar-model');
+      entity.setAttribute('gltf-model', url);
+  } catch (error) {
+      console.error('Error uploading image:', error);
+  }
+}
